17c703d390a7f01baf3cbc43eb8e5f0c7f13472a
root <root@ip-172-26-4-239.ap-southeast-1.compute.internal>
2026-05-26 16:22:04 +0000

    bug fix on migrations

--- migrations/20260510161113-create-user-bill.js
+++ migrations/20260510161113-create-user-bill.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Users',
+          model: 'users',
           key: 'id'
         },
         onDelete: 'CASCADE'
@@ -22,7 +22,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Bills',
+          model: 'bills',
           key: 'id'
         },
         onDelete: 'CASCADE'
