78d7621c50aaa56d24b97409d93f7e2f7db41c9f
Joseph Bautista <joseph.bautista@matchmeautogroup.co.uk>
2026-05-29 19:32:07 +0800

    make the rate limiter larger for testing

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
-  max: 5, 
+  max: 50, 
   message: {
     success: false,
     message: "Too many requests. Please try again later."
